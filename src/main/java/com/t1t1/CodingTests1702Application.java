a7580956549d8af3376f1b176d0e308ddd77be9c
t1t1 <cha@hanmail2.net>
2017-02-08 06:38:35 +0900

    init

---
+++ src/main/java/com/t1t1/CodingTests1702Application.java
@@ -0,0 +1,12 @@
+package com.t1t1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingTests1702Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingTests1702Application.class, args);
+	}
+}
